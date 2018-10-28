71ff93659bfa62e2c48092b66b76047d51935065
ricard <r.molina91@gmail.com>
2018-10-28 16:36:42 +0100

    Adding 2 profiles

--- src/main/java/com/test/api/apiexample/controller/InicioController.java
+++ src/main/java/com/test/api/apiexample/controller/InicioController.java
@@ -1,6 +1,8 @@
 package com.test.api.apiexample.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,9 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class InicioController {
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping
     public ResponseEntity sayHello(){
         log.info("Respondemos!");
-        return ResponseEntity.ok("Hola a todos");
+        return ResponseEntity.ok("Hola a todos"+environment.getActiveProfiles().toString());
     }
 }
